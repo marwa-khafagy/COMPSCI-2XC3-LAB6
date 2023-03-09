3e613e4b4c89e464e128b95e194a34a1a690ca28
Marwa Khafagy <marwa.a.khafagy@gmail.com>
2023-03-09 08:49:44 -0500

    started BST

---
+++ code/BST.py
@@ -0,0 +1,13 @@
+class node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+        pass
+
+
+
+class bst:
+    def __init__(self):
+        self.root = None
+        pass
