390759182c707f460963ce8e55456109813347a4
Alex Eckardt <realalexeck@gmail.com>
2023-03-13 11:59:23 -0400

    No need to plot difference, clearly visible

--- code/experiment1.py
+++ code/experiment1.py
@@ -1,31 +1,29 @@
 from lab6 import RBTree
-from BST import BST
+from bst import BST
 import timeit
 import random
 from plotting import PlotGroup
 import matplotlib.pyplot as plot
 
-# plotting functions
-
-
-# Testing function
-
-
-# run the testing function
-
+'''
+What is the average difference in the height between the two? Comment on whether you think
+there is a case where you would prefer a BST over an RBT. You do not need to empirically validate this,
+but what is your instinct on the performance of a BST insert to a RBT insert for trees of similar heights?
+In your report your experiment should include:...
+'''
 
 #-----------------------------------------------------------------------------------------------------------#
 # Trial Functions
 
-
 def run_xtrials(trials, num_of_nodes, max_value, min_value):
-
 
     bstHeight = 0
     rbHeight = 0
 
     #Loop Over
     for _ in range(trials):
+
+        #E
         bst = BST()
         rbt = RBTree()
 
@@ -34,22 +32,22 @@ def run_xtrials(trials, num_of_nodes, max_value, min_value):
             bst.insert(value)
             rbt.insert(value)
 
+        #Get Height
         bstHeight+= bst.get_height()    
         rbHeight+= rbt.get_height()
     
+    #Average
     bstHeight = bstHeight/trials
     rbHeight = rbHeight/trials
-        
-
+    
+    #Return
     return (rbHeight, bstHeight)
 
 def run_comparison_test(trials, listSizes, listMaxValue, listMinValue):
 
-
     #New Dict
     bstPlot = PlotGroup("BST", 'b')
-    rbPlot = PlotGroup("RB Tree" ,'g')
-    difPlot = PlotGroup("Difference", 'r')
+    rbPlot = PlotGroup("RB Tree" ,'r')
 
     for listSize in listSizes:
 
@@ -57,7 +55,6 @@ def run_comparison_test(trials, listSizes, listMaxValue, listMinValue):
         
         bstPlot.add_point(listSize, timesForTrial[1])
         rbPlot.add_point(listSize, timesForTrial[0], )
-        difPlot.add_point(listSize, abs(timesForTrial[0] - timesForTrial[1]))
         print("Added point at listSize ", listSize)
 
     name = f"The Difference in Height between RBTrees and BST ({trials} trails)"
@@ -68,7 +65,6 @@ def run_comparison_test(trials, listSizes, listMaxValue, listMinValue):
 
     bstPlot.plot()
     rbPlot.plot()
-    difPlot.plot()
 
     plot.legend()
     plot.show()
@@ -80,6 +76,7 @@ def run_comparison_test(trials, listSizes, listMaxValue, listMinValue):
 
 if (__name__ == "__main__"):
 
-    test = run_comparison_test(100, range(0, 1000, 1), 100, -100)
+    test = run_comparison_test(100, range(0, 1000, 10), 0, 10000) #Attempt on Unique Values
+    test = run_comparison_test(100, range(0, 1000, 10), 0, 10) #Attempt on Very Similar Values
     #test = run_comparison_test(100, BBL, range(0, 500, 5), 1000)
 #print(test)    
